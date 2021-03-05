70cdbd4bf397b51088c8363e2fb16a1664087f88
phoenixhell <phoenixrever@gmail.com>
2021-03-05 23:43:59 +0800

    2021-03-04 23：34

--- service/service-oss/src/main/java/com/phoenixhell/oss/controller/OssController.java
+++ service/service-oss/src/main/java/com/phoenixhell/oss/controller/OssController.java
@@ -137,5 +137,16 @@ export default {
   display: none;
 }
 </style>
+import request from "@/utils/request";
+
+export default {
+  uploadavatar(form) {
+    return request({
+      url: `/eduoss/fileoss/upload`,
+      method: "POST",
+      data: form
+    });
+  }
+};
 
  */
